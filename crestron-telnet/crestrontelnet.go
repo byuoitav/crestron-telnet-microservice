09d12707b7b0dbf82be8d38a7279b129a86d5e60
thailandmatt <31248374+thailandmatt@users.noreply.github.com>
2018-12-20 10:21:14 -0700

    lowercase tags

--- crestron-telnet/crestrontelnet.go
+++ crestron-telnet/crestrontelnet.go
@@ -86,7 +86,7 @@ func MonitorDMPS(dmps structs.DMPS) {
 
 				x.Timestamp, _ = time.Parse(time.RFC3339, eventParts[3]) //timestamp
 
-				x.EventTags = []string{eventParts[4], eventParts[5], eventParts[7]}
+				x.EventTags = []string{strings.Replace(strings.ToLower(eventParts[4]), " ", "-", -1), strings.Replace(strings.ToLower(eventParts[5], " ", "-", -1), strings.Replace(strings.ToLower(eventParts[7], " ", "-", -1)}
 
 				//TargetDevice
 				x.TargetDevice = events.BasicDeviceInfo{
